dbed447e3ac71b33ef2d5b03c49dbf35287b0cc1
Jake Stout <sjake@vmware.com>
2021-12-09 13:25:15 -0500

    Initial commit

---
+++ src/main/java/com/demo/tddworkshop/TddWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.demo.tddworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddWorkshopApplication.class, args);
+	}
+
+}
