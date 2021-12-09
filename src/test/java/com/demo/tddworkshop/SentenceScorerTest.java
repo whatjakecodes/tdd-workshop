31c1062cf084910c626946dc9a414b25745119df
Jake Stout <sjake@vmware.com>
2021-12-09 13:31:08 -0500

    Step 4 completed

--- src/test/java/com/demo/tddworkshop/SentenceScorerTest.java
+++ src/test/java/com/demo/tddworkshop/SentenceScorerTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 class SentenceScorerTest {
     @Test
     void lowercaseCharacters_score1PointEach() {
-        // TODO!
+        // arrange: a string with 11 lowercase characters
+
+        // act: when input is scored
+
+        // assert: output is 11
     }
 }
